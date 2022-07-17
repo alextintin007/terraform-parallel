32d617873e2c613b0826f20eeee99d5057cedb03
Xavier Tintin <79267255+alextintin007@users.noreply.github.com>
2022-07-17 18:48:34 -0500

    update destroy

--- destroy.sh
+++ destroy.sh
@@ -10,12 +10,16 @@ zones=(
     )
 
 # for test #
-names=("1" "2" "3" "4" "5")
+names=("1" "2" "3")
 #"6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "48" "49" "50" "51" "52" "53" "54" "55" "56" "57" "58" "59" "60")
-emails=("1" "2" "3" "4" "5")
+emails=("1" "2" "3")
 #"6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "48" "49" "50" "51" "52" "53" "54" "55" "56" "57" "58" "59" "60")
 
 len=${#names[@]}
+
+cd logs
+mkdir -p logs-destroy
+cd ..
 
 echo "Updating terraform providers..."
 sleep 1
@@ -23,10 +27,10 @@ sleep 1
 for (( i=0; i<${len}; i++))     
 do
     (( (( $i + 1 ) %  50) == 0 )) && sleep 100 # GCEapi limit 50 per user per 100 seconds
-    cd s/${names[$i]}
-    terraform destroy -auto-approve -var email=${emails[$i]} -var name=${names[$i]}  -var zone=${zones[$(($i % ${#zones[@]}))]}  &> ../../logs/${names[$i]}.log &
+    cd users/${names[$i]}
+    terraform destroy -auto-approve -var email=${emails[$i]} -var name=${names[$i]}  -var zone=${zones[$(($i % ${#zones[@]}))]}  &> ../../logs/logs-destroy/${names[$i]}.log &
     cd ../.. 
     clear
-    echo $i
+    echo Destroying cluster number $(($i+1))
     sleep 7
 done

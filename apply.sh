d6adb3c5f6bcafe7f09af5f746fcd8fe7c679d9d
Xavier Tintin <79267255+alextintin007@users.noreply.github.com>
2022-07-17 18:47:50 -0500

    update apply

--- apply.sh
+++ apply.sh
@@ -10,16 +10,18 @@ zones=(
     )
 
 # for test #
-names=("1" "2" "3" "4" "5")
+names=("1" "2" "3")
 #"6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "48" "49" "50" "51" "52" "53" "54" "55" "56" "57" "58" "59" "60")
-emails=("1" "2" "3" "4" "5")
+emails=("1" "2" "3")
 #"6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "48" "49" "50" "51" "52" "53" "54" "55" "56" "57" "58" "59" "60")
 
 len=${#names[@]}
 
 mkdir -p logs
-mkdir -p s
-
+cd logs
+mkdir -p logs-apply
+cd ..
+mkdir -p users
 
 echo "Updating terraform providers..."
 sleep 1
@@ -27,18 +29,17 @@ cd terraform
 terraform init 
 cd ..
 
-
 for (( i=0; i<${len}; i++))     
 do
     (( (( $i + 1 ) %  50) == 0 )) && sleep 100 # GCEapi limit 50 per user per 100 seconds
-    mkdir -p s/${names[$i]}
-    cd s/${names[$i]}
+    mkdir -p users/${names[$i]}
+    cd users/${names[$i]}
     ln -s ../../terraform/* .
     ln -s ../../terraform/.t* .
     terraform init &&
-    terraform apply -auto-approve -var email=${emails[$i]} -var name=${names[$i]}  -var zone=${zones[$(($i % ${#zones[@]}))]}  &> ../../logs/${names[$i]}.log &
+    terraform apply -auto-approve -var email=${emails[$i]} -var name=${names[$i]}  -var zone=${zones[$(($i % ${#zones[@]}))]}  &> ../../logs/logs-apply/${names[$i]}.log &
     cd ../.. 
     clear
-    echo $i
+    echo Creating cluster number $(($i+1))
     sleep 7
 done
